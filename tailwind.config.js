5d5b24867711a60a72ab21bb0d5c08d6b09d1078
sabbir-islam <sabbir3921@gmail.com>
2025-01-26 00:44:10 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          manrope: ['Manrope', 'serif']
+        },
+      },
+      colors: {
+        'custom-red': 'rgb(255, 0, 0)',
+        'custom-orange': 'rgb(255, 137, 56)',
+        'custom-pink': '#D946EF',
+      },
+    },
+  };
+  
